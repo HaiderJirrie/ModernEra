f9b8b788b68234e42426e0fd2df2608c1e0c76da
haider jirrie <haiderjirrie@gmail.com>
2024-05-15 15:46:14 +0200

    added argument path validation

--- automation.py
+++ automation.py
@@ -1,4 +1,15 @@
-# start function
-print("hello world")
+import sys, os
+    
+def validatePath():
+    if sys.argv.__len__() < 2 :
+        print("please provide a target file")
+        return
+        
+    if not os.path.exists(sys.argv[1]) or not os.path.isfile:
+        print("Target file not found")
+        return
 
-# function that checks import
+def start():
+    validatePath()
+        
+start()
