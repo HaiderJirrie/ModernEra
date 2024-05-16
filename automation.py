6342c9e72f04d05e1747d281c4be15b2d9048dbb
haider jirrie <haiderjirrie@gmail.com>
2024-05-16 13:43:27 +0200

    system exit with invalid argument instead of return

--- automation.py
+++ automation.py
@@ -3,11 +3,11 @@ import sys, os
 def validatePath():
     if sys.argv.__len__() < 2 :
         print("please provide a target file")
-        return
+        sys.exit()
         
     if not os.path.exists(sys.argv[1]) or not os.path.isfile:
         print("Target file not found")
-        return
+        sys.exit()
 
 def start():
     validatePath()
