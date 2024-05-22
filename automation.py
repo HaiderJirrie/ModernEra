0565cf7025e076a4d117222e1e4c70f8800c574c
haiderjirrie <haiderjirrie@gmail.com>
2024-05-21 22:32:02 -0700

    added pe msvcrt.dll import viewer

--- automation.py
+++ automation.py
@@ -1,15 +1,34 @@
 import sys, os, pefile
-    
-def validatePath():
-    if sys.argv.__len__() < 2 :
-        print("please provide a target file")
-        sys.exit()
-        
-    if not os.path.exists(sys.argv[1]) or not os.path.isfile:
-        print("Target file not found")
-        sys.exit()
 
-def start():
-    validatePath()
-        
-start()
+target_path = ''
+
+def validatePath():
+    global target_path
+
+    if len(sys.argv) < 2:
+        print("please provide a target file")
+        sys.exit(1)
+
+    target_path = sys.argv[1]
+
+    if not os.path.exists(target_path) or not os.path.isfile(target_path):
+        print("Target file not found")
+        sys.exit(1)
+
+    print("Target located")
+
+    #TO DO: create function that goes through the imports to determine which exploits are possible
+
+def staticAnalysis():
+    print("Analysing PE Imports")
+
+    pe =  pefile.PE(target_path)
+    for entry in pe.DIRECTORY_ENTRY_IMPORT:
+        if entry.dll.decode('utf-8').lower() == 'msvcrt.dll':
+            print(f"{entry.dll.decode('utf-8')}:")
+            for imp in entry.imports:
+                print(f"\t{imp.name.decode('utf-8') if imp.name else str(imp.ordinal)}")  
+
+# Call the functions
+validatePath()
+staticAnalysis()
