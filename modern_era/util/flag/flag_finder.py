7f5bfd94af8e66e9e5617b2a6ffd00425a0c698c
haiderjirrie <haiderjirrie@gmail.com>
2024-06-09 10:15:35 -0700

    fixed prefix index bug

--- modern_era/util/flag/flag_finder.py
+++ modern_era/util/flag/flag_finder.py
@@ -6,17 +6,18 @@ from typing import List
 class FlagFinder:
 
     def __init__(self):
-        self._flag_template = self.__set_flag_template()
-
-    def __set_flag_template():
-        if len(sys.argv) > 3:
-            return FlagTemplate(sys.argv[2], sys.argv[3])
-
-        return FlagTemplate()
+        self._flag_template = FlagFinder.__get_flag_template()
 
     @property
     def flag_template(self):
         return self._flag_template
+
+    @staticmethod
+    def __get_flag_template() -> FlagTemplate:
+        if len(sys.argv) > 3:
+            return FlagTemplate(sys.argv[2], sys.argv[3])
+        else:
+            return FlagTemplate()
 
     @staticmethod
     def __flag_found(flag: str):
@@ -24,23 +25,25 @@ class FlagFinder:
         sys.exit()
 
     def search_for_flag_in_list(self, list: List[str]):
-        flag = ""
-        flag_prefix_index = None
-
+        flag_index = None
+        
         for index, list_item in enumerate(list):
-            if not flag_prefix_index and list_item.find(self.flag_template.prefix) >= 0:
-                flag_prefix_index = index
+            flag = ""
+
+            if not flag_index and list_item.find(self.flag_template.prefix) >= 0:
+                flag_index = index
 
             last_list_item_index = len(list) - 1
 
-            if flag_prefix_index and flag_prefix_index <= last_list_item_index:
-                flag_remainder = list[flag_prefix_index:last_list_item_index]
+            if flag_index and flag_index <= last_list_item_index:
+                flag_remainder = list[flag_index:last_list_item_index]
 
                 for flag_part in flag_remainder:
                     flag += flag_part
 
+                prefix_index = list[index].find(self.flag_template.prefix)
                 suffix_index = flag.find(self.flag_template.suffix)
 
                 if suffix_index >= 0:
-                    flag = flag[: suffix_index + 1]
+                    flag = flag[prefix_index : suffix_index + 1]
                     self.__flag_found(flag)
