ace1127d6a2e4626b265a039520837edfc6ce21e
Haider Jirrie <67063086+HaiderJirrie@users.noreply.github.com>
2024-06-11 22:07:25 +0200

    Format specifiers (#4)
    * abstracted flag finder logic
    
    * flag finder fixes
    
    * added the option to define a flag template
    
    * fixed prefix index bug
    
    * added max failed attempts and swapped to smaller iterations in string fromat attack

--- modern_era/util/flag/flag_finder.py
+++ modern_era/util/flag/flag_finder.py
@@ -1,26 +1,49 @@
-from enum import Enum
+import sys
+from util.flag.flag_template import FlagTemplate
 from typing import List
 
 
-class Flag(Enum):
-    PREFIX = "flag{"
-    SUFFIX = "}"
-    BYTES = 4
+class FlagFinder:
 
+    def __init__(self):
+        self._flag_template = FlagFinder.__get_flag_template()
 
-def find_flag_in_list(list: List[str]):
-    flag_found = False
-    flag_index = 0
-    flag = ""
+    @property
+    def flag_template(self):
+        return self._flag_template
 
-    for index, list_item in enumerate(list):
-        if list_item.find(Flag.PREFIX.value) != -1:
-            flag += list_item
-            flag_index = index
-            flag_found = True
+    @staticmethod
+    def __get_flag_template() -> FlagTemplate:
+        if len(sys.argv) > 3:
+            return FlagTemplate(sys.argv[2], sys.argv[3])
+        else:
+            return FlagTemplate()
 
-        if flag_found and len(list) >= (flag_index + Flag.BYTES.value):
-            remainder = list[flag_index + 1 : flag_index + Flag.BYTES.value]
-            for part in remainder:
-                flag += part
-            return flag
+    @staticmethod
+    def __flag_found(flag: str):
+        print(flag)
+        sys.exit()
+
+    def search_for_flag_in_list(self, list: List[str]):
+        flag_index = None
+        
+        for index, list_item in enumerate(list):
+            flag = ""
+
+            if not flag_index and list_item.find(self.flag_template.prefix) >= 0:
+                flag_index = index
+
+            last_list_item_index = len(list) - 1
+
+            if flag_index and flag_index <= last_list_item_index:
+                flag_remainder = list[flag_index:last_list_item_index]
+
+                for flag_part in flag_remainder:
+                    flag += flag_part
+
+                prefix_index = list[index].find(self.flag_template.prefix)
+                suffix_index = flag.find(self.flag_template.suffix)
+
+                if suffix_index >= 0:
+                    flag = flag[prefix_index : suffix_index + 1]
+                    self.__flag_found(flag)
